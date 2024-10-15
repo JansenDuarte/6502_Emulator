504a5ec40f1c99cabef43a0cceb2a760a02c1d9e
jansenduarte1 <jansenduarte.dev@gmail.com>
2024-10-15 18:16:18 -0300

    Add Main.cpp
    Separated the inline test programs just to keep things organized

---
+++ Emulator/Main.cpp
@@ -0,0 +1,24 @@
+#include "6502.cpp"
+
+int main()
+{
+    CPU cpu;
+    Memory mem;
+    cpu.Reset(mem);
+    // inline program - load accumulator with 69
+    mem[0xFFFC] = CPU::INS_LDA_ZPX;
+    mem[0xFFFD] = 0x42;
+    mem[0x0042] = 69;
+    // inline program - load accumulator with 69
+
+    // inline program - jmp test
+    mem[0xFFFC] = CPU::INS_JMP_IND;
+    mem[0xFFFD] = 0x42;
+    mem[0x0042] = 0x69;
+    mem[0x0069] = CPU::INS_LDA_ABS;
+    mem[0x0070] = 0x66;
+    mem[0x0066] = 69;
+
+    cpu.Execute(10, mem);
+    return 0;
+}
